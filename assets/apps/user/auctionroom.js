0917d38a1e47f1fd9f49cd24269e5c4711cf029f
Ozurmba chibuike <ocbmf17@gmail.com>
2023-12-11 15:13:20 +0100

    added

--- assets/apps/user/auctionroom.js
+++ assets/apps/user/auctionroom.js
@@ -86,7 +86,9 @@ function imagesSwap(){
 fetch(apiUrl + `/Auction/GetById/${itemId}`)
 .then((res)=>res.json())
 .then((data)=>{
-      console.log(data);
+  for (let i = 0; i < data.length; i++) {
+    actiondit(data[i])
+  }
 })
 
 function actiondit(data){
